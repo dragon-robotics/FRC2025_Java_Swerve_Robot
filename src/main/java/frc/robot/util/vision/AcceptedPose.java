b1a908d72ca6c3f58dcbb7c1c99f5bd116a82064
Doug <dougdliu@gmail.com>
2025-09-01 02:10:55 -0700

    Added additional vision utilities to determine if a visual pose should be accepted or rejected

---
+++ src/main/java/frc/robot/util/vision/AcceptedPose.java
@@ -0,0 +1,8 @@
+package frc.robot.util.vision;
+
+import frc.robot.subsystems.vision.VisionIO.PoseObservation;
+
+// Record for accepted poses
+public record AcceptedPose(
+    PoseObservation poseObservation
+) implements PoseValidationResult {}
---
+++ src/main/java/frc/robot/util/vision/PoseValidationResult.java
@@ -0,0 +1,6 @@
+package frc.robot.util.vision;
+
+// Sealed interface for pose validation results
+public sealed interface PoseValidationResult 
+    permits AcceptedPose, RejectedPose {
+}
